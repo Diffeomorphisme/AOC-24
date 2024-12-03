a0d27a9ac199d34fdb8ed0d57de074e90918513b
Diffeomorphisme <nicolas.maxant@queenslab.se>
2024-12-03 21:48:20 +0100

    day3: part 2

--- app/day3/day3.py
+++ app/day3/day3.py
@@ -1,6 +1,4 @@
 import re
-import copy
-
 
 def read_file():
 	with open("day3/input.txt", "r") as file:
@@ -11,7 +9,7 @@ def first_part():
 	file_contents = read_file()
 	total = 0
 	for line in file_contents.splitlines():
-		multiplications = re.findall(r"mul\([0-9]+\,[0-9]+\)", line)
+		multiplications = re.findall(r"mul\([0-9]+,[0-9]+\)", line)
 		for multiplication in multiplications:
 			numbers = re.findall(r"[0-9]+", multiplication)
 			total += int(numbers[0]) * int(numbers[1])
@@ -22,19 +20,20 @@ def first_part():
 def second_part():
 	file_contents = read_file()
 	total = 0
-	for line in file_contents.splitlines():
-		text = line
-		dont = re.split(r"don't\(\)", text)
-		do = ""
-		for i, nope in enumerate(dont):
-			print(nope)
-			print("".join(re.split(r"do\(\)", nope)))
-			do += "".join(re.split(r"do\(\)", nope))
-		print(do)
-		multiplications = re.findall(r"mul\([0-9]+\,[0-9]+\)", do)
-		print(multiplications)
-		for multiplication in multiplications:
-			numbers = re.findall(r"[0-9]+", multiplication)
-			total += int(numbers[0]) * int(numbers[1])
+
+	matches = re.findall(
+		r"mul\([0-9]+,[0-9]+\)|don't\(\)|do\(\)",
+		file_contents
+	)
+	do = True
+	for match in matches:
+		if match == "do()":
+			do = True
+		elif match == "don't()":
+			do = False
+		else:
+			if do:
+				numbers = re.findall(r"[0-9]+", match)
+				total += int(numbers[0]) * int(numbers[1])
 
 	return total
